f8e1734325a987d5db46e836ef9b1fb1a2e0abd1
Mark Weng <mark.weng@tw.abb.com>
2022-01-19 15:45:44 +0800

    Modify the gotten plant for two organization
    For get the effective asset

--- use_case_5/get_latest_measurements.py
+++ use_case_5/get_latest_measurements.py
@@ -32,59 +32,61 @@ def run_task(settings_file=DEFAULT_SETTINGS_FILE, debug: bool = False) -> bool:
     plants = client.get_plant_list()
 
     # Iterate the plant list and print all assets therein
-    for plant in plants:
-        print('Plant {}, {}:'.format(plant['plantID'], plant['plantName']))
-        print('Assets:')
+    # for plant in plants:
 
-        values_list = []
+    # for 1212 in twabb, 10395 in tahobali
+    plant = list(filter(lambda p:p['plantID'] in [1212, 10395], plants))[0]
+    print('Plant {}, {}:'.format(plant['plantID'], plant['plantName']))
+    print('Assets:')
 
-        # Get list of assets
-        assets = client.get_asset_list(organization_id=client.organization_id, plant_id=plant['plantID'])
-        if len(assets) == 0:
-            print('No assets in this plant')
-        else:
-            asset_data = client.asset_get_asset_by_id(asset_id=assets[0]['assetID'])
-            # print('Latest measurements of Asset {}, {}:'.format(asset['assetID'], asset['assetName']))
+    # Get list of assets
+    assets = client.get_asset_list(organization_id=client.organization_id, plant_id=plant['plantID'])
+    if len(assets) == 0:
+        # print('No assets in this plant')
+        return {"msg": 'No assets in this plant'}
+    else:
+        asset_data = client.asset_get_asset_by_id(asset_id=assets[0]['assetID'])
+        # print('Latest measurements of Asset {}, {}:'.format(asset['assetID'], asset['assetName']))
 
-            if asset_data is None:
-                # If there is an error, skip to the next asset
-                continue
+        if asset_data is None:
+            # If there is an error, skip to the next asset
+            # continue
+            return {"msg": "no asset"}
 
-            vib_temp_json = filter(lambda m: m['measurementTypeCode'] in ['OverallVibration', 'SkinTemp'], asset_data['measurements'])
-            vib_temp_value = list(map(lambda j: j['measurementTypeName'].ljust(20) + ": " + j['measurementValue'] + "( {} )".format(j['healthStatus']['healthCode']), vib_temp_json))
-            total_data = ["Asset".ljust(20) + ": " + "{}".format(asset_data['assetName'])] + vib_temp_value
+        vib_temp_json = filter(lambda m: m['measurementTypeCode'] in ['OverallVibration', 'SkinTemp'], asset_data['measurements'])
+        vib_temp_value = list(map(lambda j: j['measurementTypeName'].ljust(20) + ": " + j['measurementValue'] + "( {} )".format(j['healthStatus']['healthCode']), vib_temp_json))
+        total_data = ["Asset".ljust(20) + ": " + "{}".format(asset_data['assetName'])] + vib_temp_value
 
-            print(total_data)
+        print(total_data)
 
-            # for m in asset_data['measurements']:
-            #     values_list = 
-            # # Iterate all the measurements available and print them
-            # for m in asset_data['measurements']:
+        # for m in asset_data['measurements']:
+        # # Iterate all the measurements available and print them
+        # for m in asset_data['measurements']:
 
-            #     # Print measurements that contain values
-            #     if m['measurementValue'] is not None:
-            #         value = [m['measurementTypeName'].ljust(37) + ':' + m['measurementValue'] + '(' + m['timeStamp'] + ')']
-            #         print(value)
-            #         values_list += value
-            # for asset in assets:
-            #     asset_data = client.asset_get_asset_by_id(asset_id=asset['assetID'])
-            #     print('Latest measurements of Asset {}, {}:'.format(asset['assetID'], asset['assetName']))
+        #     # Print measurements that contain values
+        #     if m['measurementValue'] is not None:
+        #         value = [m['measurementTypeName'].ljust(37) + ':' + m['measurementValue'] + '(' + m['timeStamp'] + ')']
+        #         print(value)
+        #         values_list += value
+        # for asset in assets:
+        #     asset_data = client.asset_get_asset_by_id(asset_id=asset['assetID'])
+        #     print('Latest measurements of Asset {}, {}:'.format(asset['assetID'], asset['assetName']))
 
-            #     if asset_data is None:
-            #         # If there is an error, skip to the next asset
-            #         continue
+        #     if asset_data is None:
+        #         # If there is an error, skip to the next asset
+        #         continue
 
-            #     # Iterate all the measurements available and print them
-            #     for m in asset_data['measurements']:
+        #     # Iterate all the measurements available and print them
+        #     for m in asset_data['measurements']:
 
-            #         # Print measurements that contain values
-            #         if m['measurementValue'] is not None:
-            #             value = [m['measurementTypeName'].ljust(37) + ':' + m['measurementValue'] + '(' + m['timeStamp'] + ')']
-            #             print(value)
-            #             values_list += value
-            #     print()
+        #         # Print measurements that contain values
+        #         if m['measurementValue'] is not None:
+        #             value = [m['measurementTypeName'].ljust(37) + ':' + m['measurementValue'] + '(' + m['timeStamp'] + ')']
+        #             print(value)
+        #             values_list += value
+        #     print()
 
-        # print()
+    # print()
 
     return total_data
 
